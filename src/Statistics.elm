54ee735bed1a7d50aaf6f934e7077e5b8502f74f
Martin Sigloch <f0i.git@projects.f0i.de>
2020-05-29 11:46:41 +0200

    Add functions for lists of Int

--- src/Statistics.elm
+++ src/Statistics.elm
@@ -1,7 +1,8 @@
 module Statistics exposing
-    ( avg, mean, median, percentile
+    ( avg, avgInt, mean, meanInt
+    , median, medianInt, percentile, percentileInt
     , minimum, maximum, minmax
-    , occurrences, stdDeviation
+    , occurrences, stdDeviation, stdDeviationInt
     , atLeast, atMost
     )
 
@@ -10,7 +11,8 @@ module Statistics exposing
 
 # Average
 
-@docs avg, mean, median, percentile
+@docs avg, avgInt, mean, meanInt
+@docs median, medianInt, percentile, percentileInt
 
 
 # Minimum / maximum
@@ -20,32 +22,54 @@ module Statistics exposing
 
 # Distribution
 
-@docs occurrences, stdDeviation
+@docs occurrences, stdDeviation, stdDeviationInt
 
 
-#
+# Misc
+
+@docs atLeast, atMost
 
 -}
 
 import Dict exposing (Dict)
 
 
+{-| alias for max, to make piped usage more intuitive
+
+    number |> atLeast 5
+
+-}
+atLeast : number -> number -> number
 atLeast =
     max
 
 
+{-| alias for max, to make piped usage more intuitive
+
+    number |> atMost 5
+
+-}
+atMost : number -> number -> number
 atMost =
     min
 
 
+{-| alias for List.maximum
+-}
+maximum : List number -> Maybe number
 maximum =
     List.maximum
 
 
+{-| alias for List.minimum
+-}
+minimum : List number -> Maybe number
 minimum =
     List.minimum
 
 
+{-| Calculate the mean of a list of Float
+-}
 avg : List Float -> Maybe Float
 avg list =
     case list of
@@ -54,11 +78,27 @@ avg list =
 
         _ ->
             list
-                |> List.foldl avgFolder ( 0, 0.0 )
+                |> List.foldl avgFolder ( 0, 0 )
                 |> (\( c, t ) -> t / c)
                 |> Just
 
 
+{-| Calculate the mean of a list of Int
+-}
+avgInt : List Int -> Maybe Int
+avgInt list =
+    case list of
+        [] ->
+            Nothing
+
+        _ ->
+            list
+                |> List.foldl avgFolder ( 0, 0 )
+                |> (\( c, t ) -> t // c)
+                |> Just
+
+
+avgFolder : number -> ( number, number ) -> ( number, number )
 avgFolder n ( count, total ) =
     ( count + 1, total + n )
 
@@ -67,7 +107,11 @@ mean =
     avg
 
 
-{-| Get the median of a sorted list of floats
+meanInt =
+    avgInt
+
+
+{-| Get the median of a sorted list of Float
 if the length of the list is even, the retun value is the average of the two
 values at the middle of the list.
 Returns `Nothing` if the list is empty
@@ -94,7 +138,35 @@ median sorted =
                 Nothing
 
 
+{-| Get the median of a sorted list of Int
+if the length of the list is even, the retun value is the average of the two
+values at the middle of the list.
+Returns `Nothing` if the list is empty
+-}
+medianInt : List Int -> Maybe Int
+medianInt sorted =
+    let
+        l =
+            List.length sorted
+
+        rest =
+            sorted |> List.drop ((l - 1) // 2)
+    in
+    if modBy 2 l == 1 then
+        rest |> List.head
+
+    else
+        case rest of
+            a :: b :: xs ->
+                (a + b) // 2 |> Just
+
+            _ ->
+                -- List was empty
+                Nothing
+
+
 {-| Get minimum and maximum from list
+Returns `Nothing` if list is empty
 -}
 minmax : List number -> Maybe ( number, number )
 minmax list =
@@ -110,6 +182,9 @@ minmaxFolder n ( low, high ) =
     ( min low n, max high n )
 
 
+{-| Get a `Dict` containing the numbers from the list as keys
+and the number of occurrences for each number as value
+-}
 occurrences : List number -> Dict number Int
 occurrences list =
     list |> List.foldl countFolder Dict.empty
@@ -119,6 +194,10 @@ countFolder n dict =
     dict |> Dict.update n (\c -> c |> Maybe.withDefault 0 |> (+) 1 |> Just)
 
 
+{-| Get the element at a position in percent from a list
+If the percentage doesn't exactly match an element the value is interpolated
+from the two closest elements
+-}
 percentile : Float -> List Float -> Maybe Float
 percentile p sorted =
     let
@@ -146,15 +225,77 @@ percentile p sorted =
             Nothing
 
 
+{-| Get the element at a position in percent from a list
+If the percentage doesn't exactly match an element the value is interpolated
+from the two closest elements
+-}
+percentileInt : Float -> List Int -> Maybe Int
+percentileInt p sorted =
+    let
+        l =
+            List.length sorted
+
+        pos =
+            (toFloat l - 1) * p
+
+        weight =
+            pos - toFloat (floor pos)
+
+        rest =
+            sorted |> List.drop (floor pos)
+    in
+    case rest of
+        a :: b :: xs ->
+            ((toFloat a * (1 - weight)) + (toFloat b * weight))
+                -- use truncate for same behaviour as int division (`//`)
+                |> truncate
+                |> Just
+
+        a :: [] ->
+            Just a
+
+        [] ->
+            -- List was empty
+            Nothing
+
+
+{-| Get the standard deviation of a population of Float
+-}
 stdDeviation : List Float -> Maybe Float
-stdDeviation list =
-    case mean list of
+stdDeviation population =
+    case mean population of
         Nothing ->
             Nothing
 
         Just m ->
             let
                 ( count, total ) =
-                    list |> List.foldl (\x ( n, sum ) -> ( n + 1, sum + ((x - m) ^ 2) )) ( 0, 0 )
+                    population
+                        |> List.foldl
+                            (\x ( n, sum ) ->
+                                ( n + 1, sum + ((x - m) ^ 2) )
+                            )
+                            ( 0, 0 )
             in
             Just <| sqrt (total / count)
+
+
+{-| Get the standard deviation of a population of Int
+-}
+stdDeviationInt : List Int -> Maybe Int
+stdDeviationInt population =
+    case meanInt population of
+        Nothing ->
+            Nothing
+
+        Just m ->
+            let
+                ( count, total ) =
+                    population
+                        |> List.foldl
+                            (\x ( n, sum ) ->
+                                ( n + 1, sum + ((x - m) ^ 2) )
+                            )
+                            ( 0, 0 )
+            in
+            sqrt (toFloat total / toFloat count) |> round |> Just
--- tests/StatisticsTest.elm
+++ tests/StatisticsTest.elm
@@ -2,17 +2,32 @@ module StatisticsTest exposing (..)
 
 import Dict exposing (Dict)
 import Expect exposing (Expectation, FloatingPointTolerance(..))
-import Fuzz exposing (Fuzzer, int, list, string)
+import Fuzz exposing (Fuzzer, float, intRange, list, string)
 import Statistics as Stat
 import Test exposing (..)
 
 
-testdata9 =
+float9 =
+    [ 13.0, 13, 13, 13, 14, 14, 16, 18, 21 ]
+
+
+float4 =
+    [ 1.0, 2, 4, 7 ]
+
+
+int9 =
     [ 13, 13, 13, 13, 14, 14, 16, 18, 21 ]
 
 
-testdata4 =
+int4 =
     [ 1, 2, 4, 7 ]
+
+
+{-| Int fuzzer for well defined range of Int
+as defined in [package.elm-lang.org/…/Basics#Int](https://package.elm-lang.org/packages/elm/core/latest/Basics#Int)
+-}
+int =
+    intRange (-2 ^ 31) (2 ^ 31 - 1)
 
 
 suite : Test
@@ -21,12 +36,12 @@ suite =
         [ describe "avg/mean"
             [ test "9 Floats" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.avg
                         |> Expect.equal (Just 15)
             , test "4 Floats" <|
                 \_ ->
-                    testdata4
+                    float4
                         |> Stat.mean
                         |> Expect.equal (Just 3.5)
             , test "empty" <|
@@ -35,15 +50,41 @@ suite =
                         |> Stat.avg
                         |> Expect.equal Nothing
             ]
+        , describe "avgInt/meanInt"
+            [ test "9 Ints" <|
+                \_ ->
+                    int9
+                        |> Stat.avgInt
+                        |> Expect.equal (Just 15)
+            , test "4 Ints" <|
+                \_ ->
+                    int4
+                        |> Stat.meanInt
+                        |> Expect.equal (Just 3)
+            , test "empty" <|
+                \_ ->
+                    []
+                        |> Stat.avgInt
+                        |> Expect.equal Nothing
+            ]
+        , describe "fuzz avg"
+            [ fuzz (list int) "avgInt is equal to truncated avg" <|
+                \randomList ->
+                    randomList
+                        |> List.map toFloat
+                        |> Stat.avg
+                        |> Maybe.map truncate
+                        |> Expect.equal (randomList |> Stat.avgInt)
+            ]
         , describe "median"
             [ test "9 FLoats" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.median
                         |> Expect.equal (Just 14)
             , test "4 Floats" <|
                 \_ ->
-                    testdata4
+                    float4
                         |> Stat.median
                         |> Expect.equal (Just 3)
             , test "empty" <|
@@ -55,12 +96,12 @@ suite =
         , describe "minmax"
             [ test "9 FLoats" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.minmax
                         |> Expect.equal (Just ( 13, 21 ))
             , test "4 Floats" <|
                 \_ ->
-                    testdata4
+                    float4
                         |> Stat.minmax
                         |> Expect.equal (Just ( 1, 7 ))
             , test "empty" <|
@@ -72,13 +113,13 @@ suite =
         , describe "count"
             [ test "9 FLoats" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.occurrences
                         |> Dict.toList
                         |> Expect.equal [ ( 13, 4 ), ( 14, 2 ), ( 16, 1 ), ( 18, 1 ), ( 21, 1 ) ]
             , test "4 Floats" <|
                 \_ ->
-                    testdata4
+                    float4
                         |> Stat.minmax
                         |> Expect.equal (Just ( 1, 7 ))
             , test "empty" <|
@@ -90,43 +131,72 @@ suite =
         , describe "percentile"
             [ test "50%" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.percentile 0.5
                         |> Expect.equal (Just 14)
             , test "90%" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.percentile 0.9
                         |> Expect.equal (Just 18.6)
             , test "42.125%" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.percentile 0.42125
                         |> Expect.equal (Just 13.37)
             , test "0%" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.percentile 0
                         |> Expect.equal (Just 13)
             , test "100%" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.percentile 1
                         |> Expect.equal (Just 21)
+            ]
+        , describe "fuzz percentile"
+            [ fuzz (list float) "50% equals median" <|
+                \randomList ->
+                    randomList
+                        |> Stat.percentile 0.5
+                        |> Expect.equal (randomList |> Stat.median)
+            , fuzz (list int) "50% equals medianInt" <|
+                \randomList ->
+                    randomList
+                        |> Stat.percentileInt 0.5
+                        |> Expect.equal (randomList |> Stat.medianInt)
             ]
         , describe "standard deviation"
             [ test "9 FLoats" <|
                 \_ ->
-                    testdata9
+                    float9
                         |> Stat.stdDeviation
                         |> Maybe.withDefault 0
                         |> Expect.within (Absolute 0.0001) 2.6666
             , test "4 Floats" <|
                 \_ ->
-                    testdata4
+                    float4
                         |> Stat.stdDeviation
                         |> Maybe.withDefault 0
                         |> Expect.within (Absolute 0.0001) 2.2913
+            , test "empty" <|
+                \_ ->
+                    []
+                        |> Stat.stdDeviation
+                        |> Expect.equal Nothing
+            ]
+        , describe "standard deviation Int"
+            [ test "9 FLoats" <|
+                \_ ->
+                    int9
+                        |> Stat.stdDeviationInt
+                        |> Expect.equal (Just 3)
+            , test "4 Floats" <|
+                \_ ->
+                    int4
+                        |> Stat.stdDeviationInt
+                        |> Expect.equal (Just 2)
             , test "empty" <|
                 \_ ->
                     []
